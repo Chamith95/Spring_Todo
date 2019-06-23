f3ea8568cdf474675103f79e78bf66cf1be8f6ee
Chamith95 <chamithjay@gmail.com>
2019-06-23 23:54:07 +0530

    get and delete routes

---
+++ src/main/java/com/todorest/todowebservice/todo/TodoHardCodedService.java
@@ -0,0 +1,43 @@
+package com.todorest.todowebservice.todo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardCodedService {
+
+	private static List<Todo> todos=new ArrayList();
+	private static int idCounter=0;
+	
+	static {
+		todos.add(new Todo(++idCounter,"Chamith","Learn Spring",new Date(),false));
+		todos.add(new Todo(++idCounter,"Chamith","Learn Microservices",new Date(),false));
+		todos.add(new Todo(++idCounter,"Chamith","Learn Angular",new Date(),false));
+	}
+	
+	public List<Todo> findAll(){
+		return todos;
+	}
+	
+	public Todo deleteByid(long id) {
+		Todo todo=findByid(id);
+		if(todo == null) return null;
+		if(todos.remove(todo)) {
+			return todo;	
+		};
+		return null;
+	}
+
+	public Todo findByid(long id) {
+	    for(Todo todo:todos) {
+	    	if(todo.getId() == id) {
+	    		return todo;
+	    	}
+	    }
+		return null;
+	}
+}
