a9ddb7af1de4e4f28a7ae4d3cc803e6ca6c230be
Chamith95 <chamithjay@gmail.com>
2019-06-25 01:34:44 +0530

    basic authentication

---
+++ src/main/java/com/todorest/todowebservice/basicauth/AuthenticationBean.java
@@ -0,0 +1,30 @@
+package com.todorest.todowebservice.basicauth;
+
+public class AuthenticationBean {
+
+	private String message;
+
+	public AuthenticationBean(String message) {
+		this.message=message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+}
